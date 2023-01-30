f7a97b94582217de37244eadca335db52464f217
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-30 16:05:26 -0300

    Created file LoginEntity

---
+++ src/core/entities/login-entity.ts
@@ -0,0 +1,19 @@
+import { randomUUID } from 'node:crypto'
+
+interface Login {
+  email: string
+  password: string
+}
+
+export class LoginEntity {
+  public id?: string
+  public email: string
+  public password: string
+
+  constructor({ email, password}: Login) {
+    this.id = randomUUID()
+    this.email = email
+    this.password = password
+  }
+
+}
