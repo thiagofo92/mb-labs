ac12781652cb69ee263f109b91d58c56b16bf950
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 19:12:34 -0300

    Create the file that implements the Token manager

---
+++ src/infra/services/token-service.ts
@@ -0,0 +1,21 @@
+import { type TokenServiceContract } from '@/core/services'
+import { TokenServiceDecodeError, TokenServiceCreateError } from '@/core/services/errors'
+import { left, type Either, right } from '@/shared/error/either'
+
+export class TokenService implements TokenServiceContract {
+  async create (id: string): Promise<Either<TokenServiceCreateError, string>> {
+    try {
+      return right('')
+    } catch (error: any) {
+      return left(new TokenServiceCreateError(error.message))
+    }
+  }
+
+  async decode (token: string): Promise<Either<TokenServiceDecodeError, boolean>> {
+    try {
+      return right(true)
+    } catch (error: any) {
+      return left(new TokenServiceDecodeError(error.message))
+    }
+  }
+}
