be2f25935def0bbe38bd67e472c659733d14577d
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 19:03:07 -0300

    Update

--- src/core/repositories/login-repository.ts
+++ src/core/repositories/login-repository.ts
@@ -1,9 +1,9 @@
 import { type Either } from '@/shared/error/either'
-import { type LoginValidateError, type LoginCreateError } from './error'
+import { type LoginValidateRepositoryError, type LoginCreateRepositoryError } from './error'
 import { type LoginCreateOutPutModel } from '@/app/model/output/login-output-model'
 import { type LoginEntity } from '../entities'
 
 export interface LoginContractRepository {
-  create: (input: LoginEntity) => Promise<Either<LoginCreateError, LoginCreateOutPutModel>>
-  validate: (input: LoginEntity) => Promise<Either<LoginValidateError, boolean>>
+  create: (input: LoginEntity) => Promise<Either<LoginCreateRepositoryError, LoginCreateOutPutModel>>
+  validate: (input: LoginEntity) => Promise<Either<LoginValidateRepositoryError, { id: string } | null>>
 }
