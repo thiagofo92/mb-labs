7fc1e20d602f0584613781df7269740d2a5df03d
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-30 23:35:54 -0300

    Update

--- src/core/repositories/event-repository.ts
+++ src/core/repositories/event-repository.ts
@@ -1,8 +1,9 @@
 import { type Either } from '@/shared/error/either'
 import { type EventFindAllError, type EventFindByRangeOfDateError, type EventFindByTypeError } from './error'
+import { type EventOutPutModel } from '../../app/model/output'
 
 export interface EventContractRepository {
-  findAll: () => Promise<Either<EventFindAllError, unknown>>
-  findByType: () => Promise<Either<EventFindByTypeError, unknown>>
-  findByRangeOfDate: () => Promise<Either<EventFindByRangeOfDateError, unknown>>
+  findAll: () => Promise<Either<EventFindAllError, EventOutPutModel[]>>
+  findByType: () => Promise<Either<EventFindByTypeError, EventOutPutModel[]>>
+  findByRangeOfDate: () => Promise<Either<EventFindByRangeOfDateError, EventOutPutModel[]>>
 }
