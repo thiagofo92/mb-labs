4c0b44d6e607e58ebeba4283d886b1d07a4d9223
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 11:37:39 -0300

    Add parameters to funcions By Type and By range of date

--- src/core/repositories/event-repository.ts
+++ src/core/repositories/event-repository.ts
@@ -1,9 +1,10 @@
 import { type Either } from '@/shared/error/either'
 import { type EventFindAllError, type EventFindByRangeOfDateError, type EventFindByTypeError } from './error'
 import { type EventOutPutModel } from '../../app/model/output'
+import { type EventRangeOfDateInput } from '@/app/model/input'
 
 export interface EventContractRepository {
   findAll: () => Promise<Either<EventFindAllError, EventOutPutModel[]>>
-  findByType: () => Promise<Either<EventFindByTypeError, EventOutPutModel[]>>
-  findByRangeOfDate: () => Promise<Either<EventFindByRangeOfDateError, EventOutPutModel[]>>
+  findByType: (input: string) => Promise<Either<EventFindByTypeError, EventOutPutModel[]>>
+  findByRangeOfDate: (input: EventRangeOfDateInput) => Promise<Either<EventFindByRangeOfDateError, EventOutPutModel[]>>
 }
