72d04440f8258f908db7834c17444fe002d9884e
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 11:52:33 -0300

    Create the execute function

--- src/core/repositories/order-repository.ts
+++ src/core/repositories/order-repository.ts
@@ -1,6 +1,7 @@
 import { type Either } from '@/shared/error/either'
 import { type OrderExecuteError } from './error/order-error'
+import { type OrderEntity } from '../entities'
 
 export interface OrderContractRepository {
-  execute: () => Promise<Either<OrderExecuteError, unknown>>
+  execute: (input: OrderEntity) => Promise<Either<OrderExecuteError, boolean>>
 }
