ca6119723e0bd745ba8ff95e6b6b5621b5ca6b9c
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 11:53:58 -0300

    Create the code to creat order in database

--- src/infra/repositories/prisma/order-repository.ts
+++ src/infra/repositories/prisma/order-repository.ts
@@ -1,11 +1,22 @@
 import { OrderExecuteError } from '@/core/repositories/error/order-error'
 import { type OrderContractRepository } from '@/core/repositories/order-repository'
 import { right, left, type Either } from '@/shared/error/either'
+import { connection } from './connection/connection'
+import { type OrderEntity } from '@/core/entities'
 
 export class OrderPrismaRepository implements OrderContractRepository {
-  async execute (): Promise<Either<OrderExecuteError, unknown>> {
+  async execute (input: OrderEntity): Promise<Either<OrderExecuteError, boolean>> {
     try {
-      return right('')
+      await connection.order.create({
+        data: {
+          idLogin: input.idLogin,
+          idEvent: input.idEvent,
+          payment: input.payment,
+          amount: input.amount
+        }
+      })
+
+      return right(true)
     } catch (error: any) {
       return left(new OrderExecuteError(error.message))
     }
