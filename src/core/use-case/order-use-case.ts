7c5603cae40f7635cbd0d308aa5ebf6cd94c72ce
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 21:56:53 -0300

    Created use case order

--- src/core/use-case/order-use-case.ts
+++ src/core/use-case/order-use-case.ts
@@ -1,3 +1,7 @@
-export interface OrderUseCaseContract {
+import { type OrderExecuteInput } from '@/app/model/input'
+import { type Either } from '@/shared/error/either'
+import { type OrderExecuteError } from '../repositories/error'
 
+export interface OrderUseCaseContract {
+  execute: (input: OrderExecuteInput) => Promise<Either<OrderExecuteError, boolean>>
 }
