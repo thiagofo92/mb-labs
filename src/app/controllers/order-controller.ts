98703224c290af6708da60781d76993d268beed4
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 22:51:58 -0300

    Create input params for all methods

--- src/app/controllers/order-controller.ts
+++ src/app/controllers/order-controller.ts
@@ -1,5 +1,17 @@
 import { type OrderUseCaseContract } from '@/core/use-case'
+import { type ResponseOutPutModel } from '../model/output/responde-output-model'
+import { internalError, successResponse } from '../helpers/http-helpers'
+import { type RequestInputModel } from '../model/input/request-input-model'
+import { type OrderExecuteInput } from '../model/input'
 
 export class OrderController {
   constructor (private readonly orderUseCase: OrderUseCaseContract) {}
+
+  async execute ({ body }: RequestInputModel<OrderExecuteInput>): Promise<ResponseOutPutModel> {
+    const result = await this.orderUseCase.execute(body)
+
+    if (result.isLeft()) return internalError()
+
+    return successResponse(result.value)
+  }
 }
