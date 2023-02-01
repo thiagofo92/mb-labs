e28cb01f11b726d618a237f0e555cd1d90c36895
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 22:34:17 -0300

    Created helpers for http response

---
+++ src/app/helpers/http-helpers.ts
@@ -0,0 +1,43 @@
+import { type ResponseOutPutModel } from '../model/output/responde-output-model'
+
+export function successResponse (data: any): ResponseOutPutModel {
+  return {
+    statusCode: 200,
+    data
+  }
+}
+
+export function createdReponse (data: any): ResponseOutPutModel {
+  return {
+    statusCode: 201,
+    data
+  }
+}
+
+export function internalError (): ResponseOutPutModel {
+  return {
+    statusCode: 500,
+    data: 'Internal Error'
+  }
+}
+
+export function notFound (): ResponseOutPutModel {
+  return {
+    statusCode: 404,
+    data: 'Not Found'
+  }
+}
+
+export function notAuthorized (): ResponseOutPutModel {
+  return {
+    statusCode: 401,
+    data: 'Not Authorized'
+  }
+}
+
+export function forbbinded (): ResponseOutPutModel {
+  return {
+    statusCode: 403,
+    data: 'Forbbinded'
+  }
+}
