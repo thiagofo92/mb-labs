13d469a9183cc5627765846fda7e8b0e4b49db7b
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 21:58:09 -0300

    Created return error when data not found

--- src/core/use-case/event-use-case.ts
+++ src/core/use-case/event-use-case.ts
@@ -1,5 +1,22 @@
+import { type EventOutPutModel } from '@/app/model/output'
+import { type Either } from '@/shared/error/either'
+import { type EventUseCaseNotFoundError } from './error'
+import {
+  type EventFindAllRepositoryError,
+  type EventFindByTypeRepositoryError,
+  type EventFindByRangeOfDateRepositoryError
+} from '../repositories/error'
+import { type EventRangeOfDateInput } from '@/app/model/input'
+
 export interface EventUseCaseContract {
-  findAll: () => Promise<unknown>
-  findByType: () => Promise<unknown>
-  findByRangeOfDate: () => Promise<unknown>
+  findAll: () => Promise<Either<
+  EventFindAllRepositoryError | EventUseCaseNotFoundError,
+  unknown>>
+  findByType: (type: string) => Promise<Either<
+  EventFindByTypeRepositoryError | EventUseCaseNotFoundError,
+  EventOutPutModel[]>>
+  findByRangeOfDate: (input: EventRangeOfDateInput) => Promise<Either<
+  EventFindByRangeOfDateRepositoryError | EventUseCaseNotFoundError,
+  EventOutPutModel[]
+  >>
 }
