de40a8338045efe134a949d7d3556b4b9c363a79
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 21:55:12 -0300

    Add null as a return type

--- src/core/repositories/event-repository.ts
+++ src/core/repositories/event-repository.ts
@@ -1,10 +1,14 @@
 import { type Either } from '@/shared/error/either'
-import { type EventFindAllError, type EventFindByRangeOfDateError, type EventFindByTypeError } from './error'
 import { type EventOutPutModel } from '../../app/model/output'
 import { type EventRangeOfDateInput } from '@/app/model/input'
-
+import {
+  type EventFindAllRepositoryError,
+  type EventFindByRangeOfDateRepositoryError,
+  type EventFindByTypeRepositoryError
+} from './error'
 export interface EventContractRepository {
-  findAll: () => Promise<Either<EventFindAllError, EventOutPutModel[]>>
-  findByType: (input: string) => Promise<Either<EventFindByTypeError, EventOutPutModel[]>>
-  findByRangeOfDate: (input: EventRangeOfDateInput) => Promise<Either<EventFindByRangeOfDateError, EventOutPutModel[]>>
+  findAll: () => Promise<Either<EventFindAllRepositoryError, EventOutPutModel[] | null>>
+  findByType: (input: string) => Promise<Either<EventFindByTypeRepositoryError, EventOutPutModel[] | null>>
+  findByRangeOfDate: (input: EventRangeOfDateInput) => Promise<
+  Either<EventFindByRangeOfDateRepositoryError, EventOutPutModel[] | null>>
 }
