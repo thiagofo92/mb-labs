c357714d51de44bb8df93ff842252f451b9ec512
Thigo Oliveira <thiagofo92@gmail.com>
2023-01-31 21:53:51 -0300

    Create file of use case order

--- src/app/use-case/order-use-case.ts
+++ src/app/use-case/order-use-case.ts
@@ -1,6 +1,15 @@
+import { type OrderExecuteError } from '@/core/repositories/error'
 import { type OrderContractRepository } from '@/core/repositories/order-repository'
 import { type OrderUseCaseContract } from '@/core/use-case'
+import { right, type Either, left } from '@/shared/error/either'
+import { type OrderExecuteInput } from '../model/input'
 
 export class OrderUseCase implements OrderUseCaseContract {
   constructor (private readonly orderRepository: OrderContractRepository) {}
+  async execute (input: OrderExecuteInput): Promise<Either<OrderExecuteError, boolean>> {
+    const result = await this.orderRepository.execute(input)
+    if (result.isLeft()) return left(result.value)
+
+    return right(result.value)
+  }
 }
