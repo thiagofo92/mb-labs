8a092a7a19eaf962762c82679397f6c6c280f0a2
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-02-01 17:30:57 -0300

    Add swagger route

--- src/main/server/index.ts
+++ src/main/server/index.ts
@@ -1,9 +1,13 @@
 import Express from 'express'
+import { serve as SwaggerServer, setup as SwaggerSetUp } from 'swagger-ui-express'
 import { Routers } from '../routers/routers'
+import SwaggerConfig from './../routers/swagger/swagger-config.json'
 
 const app = Express()
 const router = new Routers(Express.Router())
-app.use(Express.json())
 
-app.use(router.build.bind(router))
+app.use(Express.json())
+app.use('/api-docs', SwaggerServer, SwaggerSetUp(SwaggerConfig))
+
+app.use(router.build.apply(router))
 export { app }
