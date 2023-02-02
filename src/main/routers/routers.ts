ef64094d46c1431207bb8cd32edc1489a55ece1c
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-02-01 22:18:50 -0300

    Fix call right method - login controller

--- src/main/routers/routers.ts
+++ src/main/routers/routers.ts
@@ -2,6 +2,9 @@
 import { type Router } from 'express'
 import { EventFactoryController, LoginFactoryController, OrderFactoryController } from '../factory-controllers'
 import { ExpressAdapter } from '../adapter/express-adapter'
+import { EventByRangeOfDateMiddleware, EventByTypeMiddleware } from './middleware/schema/event-schema'
+import { loginCreateMiddleware, loginValidateMiddleware } from './middleware/schema/login-schema'
+import { OrderCreateMiddleware } from './middleware/schema/order-schema'
 
 export class Routers {
   constructor (private readonly router: Router) {}
@@ -9,19 +12,19 @@ export class Routers {
   private event (): void {
     const controller = EventFactoryController()
     this.router.get('/event/find-all', ExpressAdapter(controller.findAll.bind(controller)))
-    this.router.get('/event/by-type', ExpressAdapter(controller.findByType.bind(controller)))
-    this.router.get('/event/by-date', ExpressAdapter(controller.findByRangeOfDate.bind(controller)))
+    this.router.get('/event/by-type', EventByTypeMiddleware, ExpressAdapter(controller.findByType.bind(controller)))
+    this.router.get('/event/by-date', EventByRangeOfDateMiddleware, ExpressAdapter(controller.findByRangeOfDate.bind(controller)))
   }
 
   private order (): void {
     const controller = OrderFactoryController()
-    this.router.post('/order/execute', ExpressAdapter(controller.execute.bind(controller)))
+    this.router.post('/order/execute', OrderCreateMiddleware, ExpressAdapter(controller.execute.bind(controller)))
   }
 
   private login (): void {
     const controller = LoginFactoryController()
-    this.router.post('/login/create', ExpressAdapter(controller.create.bind(controller)))
-    this.router.post('/login/validate', ExpressAdapter(controller.create.bind(controller)))
+    this.router.post('/login/create', loginCreateMiddleware, ExpressAdapter(controller.create.bind(controller)))
+    this.router.post('/login/validate', loginValidateMiddleware, ExpressAdapter(controller.validate.bind(controller)))
   }
 
   build (): Router {
